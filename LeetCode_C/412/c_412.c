9b0a2880701ac0426ce7b1610a01e8a3ee093f70
LemonWhale <974953427@qq.com>
2023-04-07 11:20:07 +0800

    main

--- LeetCode_C/412/c_412.c
+++ LeetCode_C/412/c_412.c
@@ -11,64 +11,54 @@
 answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
 answer[i] == "Fizz" 如果 i 是 3 的倍数。
 answer[i] == "Buzz" 如果 i 是 5 的倍数。
-answer[i] == i（以字符串形式）如果上述条件全不满足。
+answer[i] == i （以字符串形式）如果上述条件全不满足。
 */
 
 char **FizzBuzz(int n)
 // int main()
 {
     // 创建二维数组
-    char **answer = (char **)(malloc((n+1) * sizeof(char)));
-    if(answer)
+    char **answer = (char **)(malloc((n + 1) * sizeof(char *)));
+
+    for (int i = 1; i <= n; i++)
     {
-        for (int i = 1; i <= n + 1; i++)
+        answer[i] = (char *)(malloc(9 * sizeof(char)));
+
+        if ((i % 3 == 0) & (i % 5 == 0))
         {
-            char* arr = (char*)(malloc(9 * sizeof(char)));
-            if(arr)
-            {
-                if ((i % 3 == 0) & (i % 5 == 0))
-                {
-                    strcpy(arr, "FizzBuzz");
-                }
-                else if ((i % 3 == 0) & (i % 5 != 0))
-                {
-                    strcpy(arr, "Fizz");
-                }
-                else if ((i % 3 != 0) & (i % 5 == 0))
-                {
-                    strcpy(arr, "Buzz");
-                }
-                else
-                {
-                    sprintf(arr, "%d", i);
-                }
-            }
-
-            answer[i] = &arr[0];
-
-            printf("answer[%d]=%s and the address is %d\n", i, answer[i], &answer[i]);
-            if (i == 1)
-                printf("%d\n", &answer[1]);
+            strcpy(answer[i], "FizzBuzz");
         }
+        else if ((i % 3 == 0) & (i % 5 != 0))
+        {
+            strcpy(answer[i], "Fizz");
+        }
+        else if ((i % 3 != 0) & (i % 5 == 0))
+        {
+            strcpy(answer[i], "Buzz");
+        }
+        else
+        {
+            sprintf(answer[i], "%d", i);
+        }
+
+        printf("answer[%d] is %s\n", i, answer[i]);
     }
 
     printf("over!\n");
+    printf("answer[1]的地址%d\n", answer[1]);
+    printf("answer的地址%d\n", sizeof(answer));
     return answer + 1;
 }
 
 int main()
 {
-    int n = 15;
-    char a = 0;
-    char b = 0;
-    char **answer= NULL;
-    char* arr = NULL;
+    char **p = FizzBuzz(15);
 
-    arr = &a;
-    answer = &arr;
-    answer= FizzBuzz(n);
-    for (int i = 0; i < n; i++)
-    {
-        printf("answer[%d] is %s\n", i, answer[i]);
-    }
+    printf("p[0] 的地址：%d\n", p[0]);
+    printf("P[0]的长度：%d\n", sizeof(p[0]));
+    for (int i = 0; i < 15; i++)
+        printf("answer[%d] = %s\n", i + 1, p[i]);
+
+    free(p);
+    return 0;
 }
